53b28279f7f43c8f54539708be257fd0f89134c7
NgocPhat003 <ngocphat2405@gmail.com>
2023-08-09 19:50:30 +0700

    Finishing rock paper scissor prototype

--- myscript.js
+++ myscript.js
@@ -22,7 +22,7 @@ function playRound(playerSelection, computerSelection){
     }
     else if(computerSelection === 'paper') {
 
-        if(playerSelectionClone ==- 'rock')
+        if(playerSelectionClone === 'rock')
             return 'You Lose! Rock is covered by Paper';
         else if(playerSelectionClone === 'paper')
             return 'Tied! Paper cancels out Paper';
@@ -39,5 +39,36 @@ function playRound(playerSelection, computerSelection){
             return 'Tied! Scissor cancels out Scissor'
     }
     
-    
 }
+
+function game(){
+    console.log('Let play Rock Paper Scissor!')
+    let compScore = 0;
+    let playerScore = 0;
+    for(let i=0; i<5; i++){
+        let playerChoice = prompt('Choose Rock, Paper or Scissor')
+        let computerChoice = getComputerChoice()
+        let result = playRound(playerChoice,computerChoice);
+        console.log('Computer chooses: ' + computerChoice);
+        console.log('Player chooses: '+ playerChoice);
+        console.log(result);
+        // check the result to count the scores
+        if(result.search('Won')!=-1)
+            playerScore++;
+        else if(result.search('Lose')!=-1)
+            compScore++;
+    }
+    // output the final scores and the winner
+    console.log('Computer score: '+ compScore)
+    console.log('Player sroce: ' + playerScore)
+        
+    if(compScore>playerScore)
+        console.log('Computer wins!')
+    else if(compScore<playerScore)
+        console.log('Player wins!')
+    else
+        console.log('Draw!!!')
+        
+}
+
+game();
